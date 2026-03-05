b36a4c0a64c6621ce676d03a26d900f62d6dfd70
Alejandro <alejanjiga@gmail.com>
2026-03-05 15:19:03 +0100

    Añadido ejemplo Login y Main

--- src/main/java/org/example/app_gestion/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.app_gestion;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloAppFXML.class, args);
-    }
-}
---
+++ src/main/java/org/example/app_gestion/MainLearn.java
@@ -0,0 +1,14 @@
+package org.example.app_gestion;
+
+public class MainLearn
+{
+    public static void main(String[] args)
+    {
+        String s1 = "hola";
+        String s2 = "mundo";
+        String s3 = s1 + " " + s2;
+
+        System.out.println("Introduzca una palabra: ");
+    }
+
+}
